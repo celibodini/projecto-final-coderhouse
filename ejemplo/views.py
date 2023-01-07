e820cf33110574d6beb06f02e95417be0cb46283
Celina Bodini <cbodini@hotmail.com>
2023-01-07 15:02:54 -0300

    actuaa

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from ejemplo.models import Celulares
 from ejemplo.forms import Buscar, CelularesForm 
 from django.views import View
+from django.views.generic import DetailView, ListView, CreateView, DeleteView, UpdateView
 
 
 def monstrar_celulares(request):
@@ -27,7 +28,7 @@ class BuscarCelular(View):
 
 class ActualizarCelular(View):
   form_class = CelularesForm
-  template_name = 'ejemplo/actualizar_celular.html'
+  template_name = 'ejemplo/actualizar_celulares.html'
   initial = {"nombre":"", "marca":"", "precio":""}
   
   # prestar atención ahora el method get recibe un parametro pk == primaryKey == identificador único
@@ -53,7 +54,7 @@ class ActualizarCelular(View):
 class AltaCelular(View):
 
     form_class = CelularesForm
-    template_name = 'ejemplo/alta_celular.html'
+    template_name = 'ejemplo/alta_celulares.html'
     initial = {"nombre":"", "marca":"", "precio":""}
 
     def get(self, request):
@@ -70,3 +71,23 @@ class AltaCelular(View):
                                                         'msg_exito': msg_exito})
         
         return render(request, self.template_name, {"form": form})
+        
+class CelularList(ListView):
+  model = Celulares
+
+class CelularCrear(CreateView):
+  model = Celulares
+  success_url = "/panel-celulares"
+  fields = ["nombre", "marca", "precio"]
+
+class CelularBorrar(DeleteView):
+  model = Celulares
+  success_url = "/panel-celulares"
+
+class CelularActualizar(UpdateView):
+  model = Celulares
+  success_url = "/panel-celulares"
+  fields = ["nombre", "marca", "precio"]
+
+class CelularDetalle(DetailView):
+  model = Celulares
--- project/urls.py
+++ project/urls.py
@@ -15,12 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ejemplo.views import (monstrar_celulares, BuscarCelular, AltaCelular, ActualizarCelular)
+from ejemplo.views import (monstrar_celulares, BuscarCelular, AltaCelular, ActualizarCelular, CelularDetalle, CelularList, CelularCrear, CelularBorrar, CelularActualizar)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('celulares/', monstrar_celulares),
-    path('celular/buscar', BuscarCelular.as_view()),
-    path('celular/alta', AltaCelular.as_view()),
-    path('celular/actualizar/<int:pk>', ActualizarCelular.as_view()),
+    path('celulares/buscar', BuscarCelular.as_view()),
+    path('celulares/alta', AltaCelular.as_view()),
+    path('celulares/actualizar/<int:pk>', ActualizarCelular.as_view()),
+    path('panel-celulares/<int:pk>/detalle', CelularDetalle.as_view()),
+    path('panel-celulares/', CelularList.as_view()),
+    path('panel-celulares/crear', CelularCrear.as_view()),
+    path('panel-celulares/<int:pk>/borrar', CelularBorrar.as_view()),
+    path('panel-celulares/<int:pk>/actualizar', CelularActualizar.as_view()),
 ]
